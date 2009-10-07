0700e0514c30b446ebf93817cfb7989fffc18d06
Victor Christensen <vc@elctech.com>
2009-10-06 17:45:50 -0700

    CHANGE: add directory code, code format

--- rails_assviz.rb
+++ rails_assviz.rb
@@ -28,7 +28,7 @@ def list_for_help(list_type)
   output=""
   spacer="                                "
   tmp_line = []
-  list.each do |f| 
+  list.each do |f|
     if f_count > 6
       output+= spacer+tmp_line.to_sentence(:last_word_connector => ", ", :two_words_connector => ", ")+"\n"
       tmp_line=[]
@@ -38,6 +38,19 @@ def list_for_help(list_type)
     f_count+=1
   end
   output+= spacer+tmp_line.to_sentence(:last_word_connector => ", ", :two_words_connector => ", ")
+end
+
+def list_directories(directory, pattern)
+  result = []
+  Dir.glob("#{directory}/*") do |file|
+    next if file[0] == ?.
+    if File.directory? file
+      result.push(*list_directories(file, pattern))
+    elsif file =~ pattern
+      result << file
+    end
+  end
+  result
 end
 
 Help = \
@@ -65,47 +78,47 @@ ARGV.each do |arg|
   stop_executing unless arg =~ /--[a-zA-Z]+ *= *[a-zA-Z\/~.]/
   split_arg = arg.split("=")
   case split_arg[0]
-  when "--in"
-    if FileTest.readable?(File.expand_path(File.join(split_arg[1],"app","models"))) 
-      @options[:in]=File.expand_path(File.join(split_arg[1],"app","models"))
-    else
-      puts "ERROR: --in must point to a Rails application's root directory\n\n"
-      stop_executing(:no_help=>true)
-    end
-  when "--out"
-    if FileTest.writable?(File.expand_path(split_arg[1]))
-      @options[:out]=File.expand_path(split_arg[1])
-    else
-      puts "ERROR: --out must be a writable directory\n\n"
-      stop_executing(:no_help=>true)
-    end
-  when "--format"
-    @options[:format] = []
-    split_arg[1].split(",").each do |f|
-      if GraphViz::FORMATS.index(f)
-        @options[:format]<<f
+    when "--in"
+      if FileTest.readable?(File.expand_path(File.join(split_arg[1], "app", "models")))
+        @options[:in]=File.expand_path(File.join(split_arg[1], "app", "models"))
       else
-        puts "ERROR: --format must be a list of allowable GraphViz formats\n\n"
+        puts "ERROR: --in must point to a Rails application's root directory\n\n"
         stop_executing(:no_help=>true)
       end
-    end
-  when "--program"
-    @options[:program] = []
-    split_arg[1].split(",").each do |p|
-      if GraphViz::PROGRAMS.index(p)
-        @options[:program]<<p
+    when "--out"
+      if FileTest.writable?(File.expand_path(split_arg[1]))
+        @options[:out]=File.expand_path(split_arg[1])
       else
-        puts "ERROR: --program must be a list of allowable GraphViz programs\n\n"
+        puts "ERROR: --out must be a writable directory\n\n"
         stop_executing(:no_help=>true)
       end
-    end
-  else
-    stop_executing
+    when "--format"
+      @options[:format] = []
+      split_arg[1].split(",").each do |f|
+        if GraphViz::FORMATS.index(f)
+          @options[:format]<<f
+        else
+          puts "ERROR: --format must be a list of allowable GraphViz formats\n\n"
+          stop_executing(:no_help=>true)
+        end
+      end
+    when "--program"
+      @options[:program] = []
+      split_arg[1].split(",").each do |p|
+        if GraphViz::PROGRAMS.index(p)
+          @options[:program]<<p
+        else
+          puts "ERROR: --program must be a list of allowable GraphViz programs\n\n"
+          stop_executing(:no_help=>true)
+        end
+      end
+    else
+      stop_executing
   end
 end
 
 #set defaults if not assigned from ARGV
-@options[:in] = (FileTest.readable?(File.expand_path(File.join(Dir.getwd,"app","models"))) ? File.expand_path(File.join(Dir.getwd,"app","models")) : stop_executing) if @options[:in].nil?
+@options[:in] = (FileTest.readable?(File.expand_path(File.join(Dir.getwd, "app", "models"))) ? File.expand_path(File.join(Dir.getwd, "app", "models")) : stop_executing) if @options[:in].nil?
 @options[:out] = (FileTest.writable?(File.expand_path(File.join(Dir.getwd))) ? File.expand_path(File.join(Dir.getwd)) : stop_executing)  if @options[:out].nil?
 @options[:format] = ["png"] if @options[:format].nil?
 @options[:program] = ["dot"] if @options[:program].nil?
@@ -114,23 +127,29 @@ end
 model_data = []
 nodes = []
 edges = []
-Dir.foreach(@options[:in]) do |file| 
-  nodes << file.gsub(".rb","").singularize unless file =~ /\A\.+/
+
+# Dir.foreach(@options[:in]) do |file| 
+results = []
+pattern = '.rb'
+models = list_directories @options[:in], /.rb$/
+models.each do |file|
+  nodes << file.gsub(".rb", "").singularize unless file =~ /\A\.+/
   unless file =~ /\A\.\.?\Z/
-    IO.readlines(File.join(@options[:in],file)).each do |line|
+    IO.readlines(file).each do |line|
       if (line =~ /\A *?((has_many)|(belongs_to)|(has_and_belongs_to_many)|(has_one))/).is_a?(Integer)
-        edges << [file.gsub(".rb",""), line]
+        edges << [file.gsub(".rb", ""), line]
       end
     end
   end
 end
+
 nodes.uniq!
 
 # build the edge relationships
 edges.each do |e|
   e[1].gsub!( /\A *?((has_many)|(belongs_to)|(has_and_belongs_to_many)|(has_one)) +:(\w+).*/m, '\6%\1')
   f = e[1].split("%")
-  e[2] = f[0].gsub(".rb","").singularize
+  e[2] = f[0].gsub(".rb", "").singularize
   e[1] = f[1]
 end
 edges.delete_if{|e|e[1]=="belongs_to"}
@@ -148,8 +167,8 @@ g.node[:color] = "#336699"
 g.node[:fontcolor] = "#ddeeff"
 
 nodes.each{|node| g.add_node(node)}
-edges.each do |edge| 
-  e=g.add_edge(edge[0],edge[2])
+edges.each do |edge|
+  e=g.add_edge(edge[0], edge[2])
   e.fontcolor = (edge[1] == "has_many" ? "#009900" : "#990000")
   e.label=edge[1]
   edge[1] == "has_one" ? e.arrowhead="none" : e.arrowhead="crow"
@@ -157,6 +176,6 @@ end
 
 @options[:program].each do |program|
   @options[:format].each do |format|
-    g.output(:output => format, :file => File.join(@options[:out],"#{program}_erd."+format), :use => program )
+    g.output(:output => format, :file => File.join(@options[:out], "#{program}_erd."+format), :use => program )
   end
 end
